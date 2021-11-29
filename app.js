040722e834b4efccd1c33d206fc80384d20d9a2b
IslemMedjahdi <ki_medjahdi@esi.dz>
2021-11-29 17:21:40 +0100

    Solving some issue with multiplication

--- app.js
+++ app.js
@@ -2,19 +2,20 @@ let input = document.getElementById("result");
 let equal = document.querySelector(".equal");
 let keys = document.querySelectorAll(".grid div");
 let theme = document.querySelector(".circle-container");
-function themeThree() {
-    theme.firstElementChild.style.transform = "translateX(1.5rem)";
-    document.documentElement.style.setProperty('--bg-color', 'hsl(268, 75%, 9%)');
-    document.documentElement.style.setProperty('--keypad-color', 'hsl(268, 71%, 12%)');
-    document.documentElement.style.setProperty('--del-bg', 'hsl(281, 89%, 26%)');
-    document.documentElement.style.setProperty('--del-shadow', 'hsl(285, 91%, 52%)');
-    document.documentElement.style.setProperty('--key-bg', 'hsl(268, 47%, 21%)');
-    document.documentElement.style.setProperty('--key-shadow', 'hsl(290, 70%, 36%)');
-    document.documentElement.style.setProperty('--text-color', 'hsl(52, 100%, 62%)');
-    document.documentElement.style.setProperty('--title-color', ' hsl(52, 100%, 62%)');
-    document.documentElement.style.setProperty('--screen-color', 'hsl(268, 71%, 12%)');
-    document.documentElement.style.setProperty('--eq-shadow', 'hsl(177, 92%, 70%)');
-    document.documentElement.style.setProperty('--toggle', 'hsl(176, 100%, 44%)');
+// Themes
+function themeOne() {
+    theme.firstElementChild.style.transform = "translateX(0rem)";
+    document.documentElement.style.setProperty('--bg-color', 'hsl(222, 26%, 31%)');
+    document.documentElement.style.setProperty('--keypad-color', 'hsl(223, 31%, 20%)');
+    document.documentElement.style.setProperty('--del-bg', 'hsl(225, 21%, 49%)');
+    document.documentElement.style.setProperty('--del-shadow', 'hsl(224, 28%, 35%)');
+    document.documentElement.style.setProperty('--key-bg', 'hsl(30, 25%, 89%)');
+    document.documentElement.style.setProperty('--key-shadow', 'hsl(28, 16%, 65%)');
+    document.documentElement.style.setProperty('--text-color', 'hsl(221, 14%, 31%)');
+    document.documentElement.style.setProperty('--title-color', ' hsl(0, 0%, 100%)');
+    document.documentElement.style.setProperty('--screen-color', 'hsl(224, 36%, 15%)');
+    document.documentElement.style.setProperty('--eq-shadow', 'hsl(25, 99%, 27%)');
+    document.documentElement.style.setProperty('--toggle', 'hsl(6, 63%, 50%)');
 }
 function themeTwo() {
     theme.firstElementChild.style.transform = "translateX(0.75rem)";
@@ -30,20 +31,21 @@ function themeTwo() {
     document.documentElement.style.setProperty('--eq-shadow', 'hsl(25, 99%, 27%)');
     document.documentElement.style.setProperty('--toggle', 'hsl(25, 98%, 40%)');
 }
-function themeOne() {
-    theme.firstElementChild.style.transform = "translateX(0rem)";
-    document.documentElement.style.setProperty('--bg-color', 'hsl(222, 26%, 31%)');
-    document.documentElement.style.setProperty('--keypad-color', 'hsl(223, 31%, 20%)');
-    document.documentElement.style.setProperty('--del-bg', 'hsl(225, 21%, 49%)');
-    document.documentElement.style.setProperty('--del-shadow', 'hsl(224, 28%, 35%)');
-    document.documentElement.style.setProperty('--key-bg', 'hsl(30, 25%, 89%)');
-    document.documentElement.style.setProperty('--key-shadow', 'hsl(28, 16%, 65%)');
-    document.documentElement.style.setProperty('--text-color', 'hsl(221, 14%, 31%)');
-    document.documentElement.style.setProperty('--title-color', ' hsl(0, 0%, 100%)');
-    document.documentElement.style.setProperty('--screen-color', 'hsl(224, 36%, 15%)');
-    document.documentElement.style.setProperty('--eq-shadow', 'hsl(25, 99%, 27%)');
-    document.documentElement.style.setProperty('--toggle', 'hsl(6, 63%, 50%)');
+function themeThree() {
+    theme.firstElementChild.style.transform = "translateX(1.5rem)";
+    document.documentElement.style.setProperty('--bg-color', 'hsl(268, 75%, 9%)');
+    document.documentElement.style.setProperty('--keypad-color', 'hsl(268, 71%, 12%)');
+    document.documentElement.style.setProperty('--del-bg', 'hsl(281, 89%, 26%)');
+    document.documentElement.style.setProperty('--del-shadow', 'hsl(285, 91%, 52%)');
+    document.documentElement.style.setProperty('--key-bg', 'hsl(268, 47%, 21%)');
+    document.documentElement.style.setProperty('--key-shadow', 'hsl(290, 70%, 36%)');
+    document.documentElement.style.setProperty('--text-color', 'hsl(52, 100%, 62%)');
+    document.documentElement.style.setProperty('--title-color', ' hsl(52, 100%, 62%)');
+    document.documentElement.style.setProperty('--screen-color', 'hsl(268, 71%, 12%)');
+    document.documentElement.style.setProperty('--eq-shadow', 'hsl(177, 92%, 70%)');
+    document.documentElement.style.setProperty('--toggle', 'hsl(176, 100%, 44%)');
 }
+// local storage of the theme
 if (window.localStorage.getItem("theme")){
     theme.classList.add(window.localStorage.getItem("theme"));
     if (theme.classList.contains("three")){
@@ -55,6 +57,7 @@ if (window.localStorage.getItem("theme")){
 }else{
     theme.classList.add("one");
 }
+// active theme
 theme.onclick = () =>{
     if (theme.classList.contains("one")){
         theme.classList.add("two");
@@ -76,6 +79,7 @@ theme.onclick = () =>{
         window.localStorage.setItem("theme","one");
     }
 }
+// calculator
 keys.forEach(elem => {
     if (elem.classList.contains("del")){
         elem.onclick = () =>{
@@ -95,6 +99,11 @@ keys.forEach(elem => {
     }
     else if (elem.classList.contains("equal")){
         elem.onclick = () => {
+        if (input.classList.contains("active")){
+            input.value = "";
+            input.classList.remove("active");
+        }
+        else{
             try {
                 input.value = eval(input.value);
             }
@@ -105,11 +114,9 @@ keys.forEach(elem => {
                     throw e;
                 }
             }
-            keys.forEach(elem=> {
-                input.classList.add("active");
-            })
+            input.classList.add("active");
         }
-        
+    }
     }
     else{
         elem.onclick = () => {
